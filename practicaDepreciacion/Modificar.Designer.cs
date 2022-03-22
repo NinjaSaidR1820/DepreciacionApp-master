2fe5cde7bc59832f4296d5b585c1cc8bb4f77edc
NinjaSaidR1820 <saidrivas1820@hotmail.com>
2022-03-22 09:14:58 -0600

    Modificar

---
+++ practicaDepreciacion/Modificar.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace practicaDepreciacion
+{
+    partial class Modificar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.txtVidaU = new System.Windows.Forms.TextBox();
+            this.txtValorR = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(83, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Vida util";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Valor residual";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(111, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Valor";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Font = new System.Drawing.Font("Mongolian Baiti", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nombre.Location = new System.Drawing.Point(90, 50);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(78, 20);
+            this.Nombre.TabIndex = 13;
+            this.Nombre.Text = "Nombre";
+            // 
+            // txtVidaU
+            // 
+            this.txtVidaU.Location = new System.Drawing.Point(189, 191);
+            this.txtVidaU.Name = "txtVidaU";
+            this.txtVidaU.Size = new System.Drawing.Size(100, 20);
+            this.txtVidaU.TabIndex = 12;
+            // 
+            // txtValorR
+            // 
+            this.txtValorR.Location = new System.Drawing.Point(189, 150);
+            this.txtValorR.Name = "txtValorR";
+            this.txtValorR.Size = new System.Drawing.Size(100, 20);
+            this.txtValorR.TabIndex = 11;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(189, 103);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 10;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(189, 50);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(115, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 53);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Modificar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Modificar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.ClientSize = new System.Drawing.Size(382, 370);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.txtVidaU);
+            this.Controls.Add(this.txtValorR);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Modificar";
+            this.Text = "Modificar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.TextBox txtVidaU;
+        private System.Windows.Forms.TextBox txtValorR;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button button1;
+    }
+}
